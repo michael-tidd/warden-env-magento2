783c629c4cff081d9f9e706ab4cc0273e24513c2
Michael Tidd <mwtidd@envisa.com>
2020-09-04 21:38:49 -0400

    Support Enterprise

--- webroot/app/etc/env.php.init.php
+++ webroot/app/etc/env.php.init.php
@@ -5,14 +5,33 @@ return [
             'default' => [
                 'frontend_options' => [
                     'write_control' => false
+                ],
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => 'redis',
+                    'database' => '0',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '1',
+                    'compression_lib' => ''
                 ]
             ],
             'page_cache' => [
                 'frontend_options' => [
                     'write_control' => false
+                ],
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => 'redis',
+                    'database' => '1',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '0',
+                    'compression_lib' => ''
                 ]
             ]
-        ]
+        ],
+        'allow_parallel_generation' => false
     ],
     'system' => [
         'default' => [
@@ -118,7 +137,123 @@ return [
                     'password_lifetime' => '90',
                     'password_is_forced' => '1'
                 ]
+            ],
+            'twofactorauth' => [
+                'general' => [
+                    'force_providers' => [
+                        'google'
+                    ]
+                ]
             ]
         ]
+    ],
+    'backend' => [
+        'frontName' => 'backend'
+    ],
+    'queue' => [
+        'amqp' => [
+            'host' => 'rabbitmq',
+            'port' => '5672',
+            'user' => 'guest',
+            'password' => 'guest',
+            'virtualhost' => '/'
+        ],
+        'consumers_wait_for_messages' => 0
+    ],
+    'db' => [
+        'connection' => [
+            'indexer' => [
+                'host' => 'db',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => 'magento',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'persistent' => null
+            ],
+            'default' => [
+                'host' => 'db',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => 'magento',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ],
+        'table_prefix' => ''
+    ],
+    'crypt' => [
+        'key' => 'e79a6af6f7bf3dd7f0a978a02e4f854c'
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'http_cache_hosts' => [
+        [
+            'host' => 'varnish',
+            'port' => '80'
+        ]
+    ],
+    'session' => [
+        'save' => 'redis',
+        'redis' => [
+            'host' => 'redis',
+            'port' => '6379',
+            'password' => '',
+            'timeout' => '2.5',
+            'persistent_identifier' => '',
+            'database' => '2',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '1',
+            'max_concurrency' => '20',
+            'break_after_frontend' => '5',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '0',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000',
+            'sentinel_master' => '',
+            'sentinel_servers' => '',
+            'sentinel_connect_retries' => '5',
+            'sentinel_verify_master' => '0'
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 0,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 0,
+        'target_rule' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
     ]
 ];
